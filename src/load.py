b250502862e9ac24ff7638d3bebc4f62a64c9e3b
Tej Harish More <tm7460@g.rit.edu>
2026-05-30 06:03:39 -0400

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,8 +1,14 @@
 import pandas as pd
+from database import load_dataframe_to_db
+
 
 def load_to_csv(df: pd.DataFrame, output_path: str):
-    """
-    Save processed data to CSV.
-    """
+
     df.to_csv(output_path, index=False)
+
     print(f"Dataset saved to {output_path}")
+
+
+def load_to_database(df: pd.DataFrame):
+
+    load_dataframe_to_db(df)
