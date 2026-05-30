ca44c53af20aaeafc10f23f49a054aa7372689e1
Tej Harish More <tm7460@g.rit.edu>
2026-05-30 05:53:53 -0400

    Update load.py

--- src/load.py
+++ src/load.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+def load_to_csv(df: pd.DataFrame, output_path: str):
+    """
+    Save processed data to CSV.
+    """
+    df.to_csv(output_path, index=False)
+    print(f"Dataset saved to {output_path}")
